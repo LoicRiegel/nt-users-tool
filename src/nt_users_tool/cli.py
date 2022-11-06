1983a3943d67bd1f3769f824d412b70491fbc270
Loïc Riegel <loic.riegel@outlook.fr>
2022-11-06 18:32:51 +0100

    Add security check to prevent code injection + quick improvements in cli.py

--- src/nt_users_tool/cli.py
+++ src/nt_users_tool/cli.py
@@ -1,11 +1,14 @@
-from time import perf_counter
-from openpyxl import load_workbook
 from sys import exit
+from time import perf_counter
 
+from openpyxl import load_workbook
+
+from nt_users_tool.nt_user_info import check_nt_user
 from nt_users_tool.read_config import read_config_file, get_input_file
 from nt_users_tool.net_commands import extract_all_nt_user_info, get_all_nt_user_string
 from nt_users_tool.excel_processing import create_results_sheets, fill_all_sheets, read_nt_users
 from nt_users_tool.constants import SHEET_INPUT, CONFIG_FILE_PATH
+
 
 def main() -> int:
     """The main functions takes an excel file with a list of nt_users as input.
@@ -13,49 +16,66 @@ def main() -> int:
     It also creates a table on the last sheet to easily sort through.
     Everything is saved on the same input document.
 
-    :return: Returns 0 for standard practice.
+    :return: 1 if the script executed successfully, 1 if an error was raised.
     """
     print("Starting nt_user verification.")
+
+    # Read configuration file
     try:
         dict_conf = read_config_file()
     except FileNotFoundError:
         print(f"Configuration file {CONFIG_FILE_PATH} cannot be found in current directory. Make sure it is present.")
-        exit()
+        return 1
+
+    # Parse configuration and open Excel workbook
     print(f"Opening {CONFIG_FILE_PATH}.")
     input = get_input_file(dict_conf)
     try:
         wb = load_workbook(input, read_only=False)
     except FileNotFoundError:
         print(f"{input} cannot be found in current directory. Make sure it is present.")
-        exit()
+        return 1
     except PermissionError:
         print(f"{input} cannot be opened. Make sure it is not open in another program.")
-        exit()
+        return 1
     print(f"Opening {input}.")
+
+    # Try to read the list of NT users in the input sheet
     try:
         ws = wb[SHEET_INPUT]
     except KeyError:
         print(f"Could not open the worksheet named {SHEET_INPUT} inside {input}. Make sure the nt users are listed in this sheet.")
-        exit()
+        return 1
+
     print(f"Reading worksheet {SHEET_INPUT}.")
-    nt_users_list = read_nt_users(ws)
+    nt_users = read_nt_users(ws)
+    for nt_user in nt_users:
+        check_nt_user(nt_user)  # Security check
+
     print("Gathering information from the network...")
     start = perf_counter()
-    net_command_response_list = get_all_nt_user_string(nt_users_list)
+    net_command_responses = get_all_nt_user_string(nt_users)
     end = perf_counter()
-    print(f"Information gathered in {end-start} seconds. Filtering.")
-    nt_user_info_list = extract_all_nt_user_info(net_command_response_list)
-    print(f"Uploading results to {input}.")
+    print(f"Information gathered in {end-start:.2f} seconds.")
+
+    print("Filtering.")
+    nt_user_info_list = extract_all_nt_user_info(net_command_responses)
+
+    print(f"Write results to {input}.")
     create_results_sheets(wb)
-    fill_all_sheets(wb,nt_user_info_list)
+    fill_all_sheets(wb, nt_user_info_list)
+
+    # Save updated Excel workbook
     try:
         wb.save(input)
     except PermissionError:
         print(f"Could not save {input}. Make sure it is not open in another program.")
-        exit()
+        return 1
+
     wb.close()
     print(f"Done. {input} saved with changes.")
     return 0
 
+
 if __name__ == '__main__':
-    main()
+    exit(main())
--- src/nt_users_tool/nt_user_info.py
+++ src/nt_users_tool/nt_user_info.py
@@ -2,7 +2,26 @@ from typing import NamedTuple
 from datetime import date
 from enum import Enum
 
-from nt_users_tool.constants import DAYS_EXPIRED_LIMIT, DAYS_EXPIRING_15_LIMIT, DAYS_EXPIRING_30_LIMIT, DAYS_EXPIRING_60_LIMIT
+from nt_users_tool.constants import (
+    NT_USER_MAX_LEN,
+    DAYS_EXPIRED_LIMIT,
+    DAYS_EXPIRING_15_LIMIT,
+    DAYS_EXPIRING_30_LIMIT,
+    DAYS_EXPIRING_60_LIMIT;
+)
+
+_DATE_NOW = date.today()
+
+
+class InvalidUserNameError(Exception):
+    """Raised for invalid NT user names."""
+
+    def __init__(self, user_name) -> None:
+        self.user_name = user_name
+    
+    def __str__(self) -> str:
+        return f"{self.user_name} is not valid!"
+
 
 class NTUserInfo(NamedTuple):
     """Dataclass for a user of the bosch network.
@@ -24,15 +43,13 @@ class NTUserStatus(Enum):
     EXPIRING_60_DAYS = "expiring 60 days"
     VALID = "valid"
 
-DATE_NOW = date.today()
-
 def evaluate_user_status(nt_user_info: NTUserInfo) -> NTUserStatus:
     """Computes the given nt user status with regard to its expiration date.
 
     :param nt_user_info: The NT User that will be evaluated.
     :return: A NTUserStatus enum to give to the excel processing module.
     """
-    days_to_expiration = (nt_user_info.expiration_date - DATE_NOW).days
+    days_to_expiration = (nt_user_info.expiration_date - _DATE_NOW).days
     if days_to_expiration <= DAYS_EXPIRED_LIMIT:
         return NTUserStatus.EXPIRED
     elif DAYS_EXPIRED_LIMIT < days_to_expiration <= DAYS_EXPIRING_15_LIMIT:
@@ -43,3 +60,14 @@ def evaluate_user_status(nt_user_info: NTUserInfo) -> NTUserStatus:
         return NTUserStatus.EXPIRING_60_DAYS
     else:
         return NTUserStatus.VALID 
+
+
+def check_nt_user(nt_user:str) -> None:
+    """Check the validity of a user name.
+    
+    :param user_name: user name to check
+    :raises : if the given user name is invalid.
+    """
+    # Check that the length of the user name
+    if len(nt_user) > NT_USER_MAX_LEN:
+        raise IndentationError(nt_user)
